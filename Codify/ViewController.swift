5b926cb6f4df9c19a093263a41e391a0f8dcecbf
William Mora <wilmor24@gmail.com>
2015-08-09 09:35:12 -0300

    Initial Commit

---
+++ Codify/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Codify
+//
+//  Created by William Mora on 9/8/15.
+//  Copyright (c) 2015 nispok. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
