bbb3e5511b5736cb9846485b79792c5aa0ca1497
William Mora <wilmor24@gmail.com>
2015-08-09 21:12:39 -0300

    authorization service

---
+++ Codify/AuthorizationService.swift
@@ -0,0 +1,35 @@
+//
+//  AuthorizationService.swift
+//  Codify
+//
+//  Created by William Mora on 9/8/15.
+//  Copyright (c) 2015 nispok. All rights reserved.
+//
+
+import Foundation
+
+class AuthorizationService {
+    
+    let clientIdPlistKey = "GitHub Client ID"
+    let clientSecretPlistKey = "GitHub Client Secret"
+    let clientIdKey = "client_id"
+    let clientSecretKey = "client_secret"
+    let redirectURIKey = "redirect_uri"
+    let scopeKey = "scope"
+    let state = "state"
+    
+    func authorizationRequest() -> NSURLRequest {
+        return NSURLRequest(URL: authorizationRequestURL())
+    }
+    
+    func authorizationRequestURL() -> NSURL {
+        let credentialsPath = NSBundle.mainBundle().pathForResource("Credentials", ofType: "plist")
+        let credentials = NSDictionary(contentsOfFile: credentialsPath!)!
+        let clientId = credentials[clientIdPlistKey] as! NSString
+        let clientSecret = credentials[clientSecretPlistKey] as! NSString
+        let baseURL = "https://github.com/login/oauth/authorize"
+        let url = NSURL(string: "\(baseURL)?\(clientIdKey)=\(clientId)&\(clientSecretKey)=\(clientSecret)")
+        return url!
+    }
+    
+}
