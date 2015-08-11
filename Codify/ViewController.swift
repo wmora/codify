085b2ccdd1612d7d03492ec0b7098400aaade126
William Mora <wilmor24@gmail.com>
2015-08-10 22:10:29 -0300

    open browser for authorization

--- Codify/ViewController.swift
+++ Codify/ViewController.swift
@@ -10,16 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var authorizationService = AuthorizationService()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func loginPressed(sender: AnyObject) {
+        let authorizationUrl = authorizationService.authorizationRequestURL()
+        UIApplication.sharedApplication().openURL(authorizationUrl)
     }
-
 
 }
 
