5e3c15d6adb561c71aa3ed8d19ee2ff0632bb382
William Mora <wilmor24@gmail.com>
2015-08-14 20:53:59 -0300

    CreateIssueViewController

---
+++ Codify/CreateIssueViewController.swift
@@ -0,0 +1,20 @@
+//
+//  CreateIssueViewController.swift
+//  Codify
+//
+//  Created by William Mora on 14/8/15.
+//  Copyright (c) 2015 nispok. All rights reserved.
+//
+
+import UIKit
+
+class CreateIssueViewController: UIViewController {
+    
+    @IBOutlet weak var issueTitle: UITextField!
+    @IBOutlet weak var issueBody: UITextView!
+    
+    @IBAction func createIssue(sender: AnyObject) {
+
+    }
+    
+}
